cfa546366c4e8b69def2ff6f2ca27c9f23899faa
pahalsonu <pahalsonu@gmail.com>
2020-11-22 15:29:43 +0530

    state completed

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 
 import './App.css';
 import NavBar from './components/Navbar'
+import Users from './Users';
 class App extends React.Component {
   render() {
     return (
       <div>
         <NavBar />
+        <Users />
         </div>
       
     )
